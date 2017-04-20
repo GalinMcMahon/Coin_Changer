075ebecd4a85cc729bf8f4a86e595042747dff26
Aubrey Masten and Galin McMahon <galin@>
2017-04-20 11:09:56 -0700

    refactor code to use an each loop

--- lib/coins.rb
+++ lib/coins.rb
@@ -3,27 +3,36 @@ class Fixnum
     change = self
     number_of_coins = [] #final number of coins
 
-    if change.>=(25) #quarters code
-      number_of_coins.push((change-change.%(25))/25)
-      change = change.-(change-change.%(25))
-    else
-      number_of_coins.push(0)
-    end
-    if change.>=(10) #dimes code
-      number_of_coins.push((change-change.%(10))/10)
-      change = change.-(change-change.%(10))
-    else
-      number_of_coins.push(0)
-    end
-    if change.>=(5) #nickels code
-      number_of_coins.push((change-change.%(5))/5)
-      change = change.-(change-change.%(5))
-    else
-      number_of_coins.push(0) # nickels placeholder
-    end
-      number_of_coins.push(change) #number of pennies remaining
+    # if change.>=(25) #quarters code
+    #   number_of_coins.push((change-change.%(25))/25)
+    #   change = change.-(change-change.%(25))
+    # else
+    #   number_of_coins.push(0)
+    # end
+    # if change.>=(10) #dimes code
+    #   number_of_coins.push((change-change.%(10))/10)
+    #   change = change.-(change-change.%(10))
+    # else
+    #   number_of_coins.push(0)
+    # end
+    # if change.>=(5) #nickels code
+    #   number_of_coins.push((change-change.%(5))/5)
+    #   change = change.-(change-change.%(5))
+    # else
+    #   number_of_coins.push(0) # nickels placeholder
+    # end
 
+    coins = [25,10,5]
+    coins.each() do |coin|
+      # if change.>=(coin)
+        number_of_coins.push((change-change.%(coin))/coin)
+        change = change.-(change-change.%(coin))
+      # else
+        # number_of_coins.push(0) # nickels placeholder
+      # end
+    end
+    number_of_coins.push(change) #number of pennies remaining
 
-  number_of_coins
+    number_of_coins
   end
 end
