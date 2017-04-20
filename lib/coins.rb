48b923617f19c309b0e08c27205247857a90ff6b
Aubrey Masten and Galin McMahon <aubrey@>
2017-04-20 14:02:13 -0700

    add code to indicate whether the program ran out of any coin types

--- lib/coins.rb
+++ lib/coins.rb
@@ -23,23 +23,26 @@ class Fixnum
 
     #available - quarters: 78 dimes: 13 nickels: 100
     available_coins = [78,13,100]
+    unavailable_coins = []
 
     coin_values = [25,10,5]
     coin_values.each_with_index() do |coin_value, index|
       total_necessary_pennies = pennies-pennies.%(coin_value)
       total_necessary_coins = total_necessary_pennies/coin_value
 
-      if total_necessary_coins.<=(available_coins[index])
+      if total_necessary_coins.<(available_coins[index])
         final_coins_array.push(total_necessary_coins)
         pennies = pennies.-(total_necessary_pennies)
+        unavailable_coins.push(false)
       else
         final_coins_array.push(available_coins[index]) # takes all avaible quarters out, passes rem pennies
         pennies = pennies.-((coin_value).*(available_coins[index])) # takes away available quarter total from the needed coin amt, passes running total back to self
+        unavailable_coins.push(true)
       end
 
     end
     final_coins_array.push(pennies) #number of pennies remaining
 
-    final_coins_array
+    [final_coins_array, unavailable_coins]
   end
 end
--- spec/coins_spec.rb
+++ spec/coins_spec.rb
@@ -3,34 +3,34 @@ require('./lib/coins')
 
 describe('Fixnum#coins') do
   it("returns the number of pennies equal to user input") do
-    expect((4).coins()).to(eq([0, 0, 0, 4]))
+    expect((4).coins()).to(eq([[0, 0, 0, 4],[false, false, false]]))
   end
 
   it("returns the number of nickels equal to user input if user input is evenly divisible into nickels") do
-    expect((5).coins()).to(eq([0, 0, 1, 0]))
+    expect((5).coins()).to(eq([[0, 0, 1, 0],[false, false, false]]))
   end
 
   it("returns the number of dimes equal to user input if user input is evenly divisible into dimes") do
-    expect((20).coins()).to(eq([0, 2, 0, 0]))
+    expect((20).coins()).to(eq([[0, 2, 0, 0],[false, false, false]]))
   end
 
   it("returns the number of quarters equal to user input if user input is evenly divisible into quarters") do
-    expect((75).coins()).to(eq([3, 0, 0, 0]))
+    expect((75).coins()).to(eq([[3, 0, 0, 0],[false, false, false]]))
   end
 
   it("returns the number of quarters and pennies equal to user input if user input is not evenly divisible into quarters") do
-    expect((26).coins()).to(eq([1, 0, 0, 1]))
+    expect((26).coins()).to(eq([[1, 0, 0, 1],[false, false, false]]))
   end
 
   it("returns the number of dimes and pennies equal to user input if user input is not evenly divisible into dimes") do
-    expect((11).coins()).to(eq([0, 1, 0, 1]))
+    expect((11).coins()).to(eq([[0, 1, 0, 1],[false, false, false]]))
   end
 
   it("returns the number of nickels and pennies equal to user input if user input is not evenly divisible into dimes") do
-    expect((6).coins()).to(eq([0, 0, 1, 1]))
+    expect((6).coins()).to(eq([[0, 0, 1, 1],[false, false, false]]))
   end
 
   it("returns the number of coins equal to user input if user input exceeds number of maximum available quarters, nickels or dimes.  The system will distribute excess cents.") do
-    expect((2500).coins()).to(eq([78, 13, 84, 0]))
+    expect((2500).coins()).to(eq([[78, 13, 84, 0],[true, true, false]]))
   end
 end
